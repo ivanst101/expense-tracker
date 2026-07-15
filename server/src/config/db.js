19ce507035db436db7a210f2ed13287168b3131a
ivanst101 <ivanstojkovic177@gmail.com>
2026-07-15 19:11:26 +0200

    Rewrite code to use ESModules

--- server/src/config/db.js
+++ server/src/config/db.js
@@ -1,18 +1,19 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
-
-const databaseUrl = process.env.DATABASE_URL.replace(
-  "<db_password>",
-  process.env.DATABASE_PASSWORD,
-);
+import mongoose from "mongoose";
 
 async function connectDB() {
   try {
-    const db = await mongoose.connect(databaseUrl);
+    const databaseUrl = process.env.DATABASE_URL.replace(
+      "<db_password>",
+      process.env.DATABASE_PASSWORD,
+    );
+
+    await mongoose.connect(databaseUrl);
+
     console.log("Database connected successfuly!");
   } catch (error) {
     console.error(`Error: ${error.message}`);
+    process.exit(1);
   }
 }
 
-module.exports = connectDB;
+export default connectDB;
--- server/src/server.js
+++ server/src/server.js
@@ -1,12 +1,10 @@
-require("dotenv").config();
-
-const app = require("./app");
-const connectDB = require("./config/db");
+import "dotenv/config";
+import app from "./app.js";
+import connectDB from "./config/db.js";
 
 const PORT = process.env.PORT || 3000;
 
-connectDB().then(() => {
-  app.listen(PORT, () => {
-    console.log(`Server is listening on a port ${PORT}`);
-  });
+await connectDB();
+app.listen(PORT, () => {
+  console.log(`Server is listening on a port ${PORT}`);
 });
