eee115b3c1d0fac8de96f340ac1bca4c9e215cff
ivanst101 <ivanstojkovic177@gmail.com>
2026-08-09 14:55:46 +0200

    Work on Header component

--- client/src/components/layouts/Header.tsx
+++ client/src/components/layouts/Header.tsx
@@ -1,3 +1,17 @@
+import { Plus } from "lucide-react";
+
 export default function Header() {
-  return <header>Header</header>;
+  return (
+    <header className="p-6 flex flex-col lg:items-center justify-start align-middle gap-4 sm:flex-row sm:items-start">
+      <div className="md:flex-1">
+        <h1 className="text-4xl sm:text-2xl text-background">Dashboard</h1>
+        <p>Welcome back, Alex. Here's your financial overview.</p>
+      </div>
+      <button className="bg-linear-to-r from-gradient-white-green to-gradient-solid-green text-white flex justify-end align-middle p-2 rounded-md">
+        <Plus />
+        Quick add
+      </button>
+      <img className="rounded-3xl" src="person.png" alt="person" />
+    </header>
+  );
 }
