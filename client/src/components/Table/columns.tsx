e329c08c3b7de98c7f17e109b23cc54686479264
ivanst101 <ivanstojkovic177@gmail.com>
2026-08-13 20:36:56 +0200

    Add pagination on expense table

--- client/src/components/Table/columns.tsx
+++ client/src/components/Table/columns.tsx
@@ -5,8 +5,25 @@ import { type Expense } from "../../types/expensesType";
 const columnHelper = createColumnHelper<DataTableFeatures, Expense>();
 
 export const columns = columnHelper.columns([
+  columnHelper.accessor("date", {
+    header: "Date",
+    cell: ({ row }) => {
+      const date = new Date(row.getValue("date"));
+      const formatted = new Intl.DateTimeFormat("sr-RS").format(date);
+      return <div>{formatted}</div>;
+    },
+  }),
   columnHelper.accessor("title", { header: "Title" }),
-  columnHelper.accessor("amount", { header: "Amount" }),
   columnHelper.accessor("category", { header: "Category" }),
-  columnHelper.accessor("date", { header: "Date" }),
+  columnHelper.accessor("amount", {
+    header: () => <div className="text-right">Amount</div>,
+    cell: ({ row }) => {
+      const amount = parseFloat(row.getValue("amount"));
+      const formatted = new Intl.NumberFormat(navigator.language, {
+        style: "currency",
+        currency: "RSD",
+      }).format(amount);
+      return <div className="text-right font-medium">{formatted}</div>;
+    },
+  }),
 ]);
